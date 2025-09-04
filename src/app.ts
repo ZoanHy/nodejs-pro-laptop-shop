85fada0e6baee7c32db9bd37f954e031f4154d61
Minh Huy Le Doan <huyleezoan@gmail.com>
2025-09-04 10:59:39 +0700

    feat(config): config cho template engine

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express from "express";
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
+
+// config template engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+console.log(__dirname);
 
 app.get("/", (req, res) => {
-  res.send("Hello World! update");
+  res.render("home");
 });
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
-  console.log("env port: ", process.env.PORT);
 });
